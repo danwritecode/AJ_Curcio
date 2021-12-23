2ba0adb5e8be7123bd967a041b0865c5c12e8bf1
nelsynelz <dnelson4993@outlook.com>
2021-12-22 21:25:40 -0500

    services selector mobile friendly

--- components/servicesselector.vue
+++ components/servicesselector.vue
@@ -2,13 +2,8 @@
   <div>
     <div class="sm:hidden">
       <label for="tabs" class="sr-only">Select a tab</label>
-      <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
       <select id="tabs" name="tabs" class="block w-full focus:ring-red-500 focus:border-red-500 border-gray-300 rounded-md">
-        <option>Buying Home</option>
-
-        <option>Selling Home</option>
-
-        <option selected>Mortgage</option>
+        <option v-for="service in services" :key="service.serviceName">{{ service.serviceName }}</option>
       </select>
     </div>
     <div class="hidden sm:block">
@@ -19,25 +14,12 @@
       </nav>
     </div>
   </div>
-  <div class="mt-2 absolute left-0 rounded-lg border border-gray-200 bg-white p-6 md:grid md:grid-cols-4 md:gap-2">
-    <div v-for="field in selectedService.fields" :key="field.fieldName" class="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-red-400 focus-within:border-red-400">
+  <div class="mt-4 sm:mt-2 sm:absolute left-0 rounded-lg sm:border sm:border-gray-200 bg-white sm:p-6 sm:grid sm:grid-cols-4 sm:gap-2">
+    <div v-for="field in selectedService.fields" :key="field.fieldName" class="mb-4 sm:mb-0 relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-red-400 focus-within:border-red-400">
       <label for="name" class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900">{{ field.fieldName }}</label>
       <input type="text" name="name" id="name" class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" :placeholder="field.placeholder">
     </div>
-
-    <!-- <div class="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-red-400 focus-within:border-red-400">
-      <label for="name" class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900">Name</label>
-      <input type="text" name="name" id="name" class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Jane Doe">
-    </div>
-    <div class="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-red-400 focus-within:border-red-400">
-      <label for="name" class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900">Email</label>
-      <input type="text" name="name" id="name" class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="jdoe@email.com">
-    </div>
-    <div class="relative border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-red-400 focus-within:border-red-400">
-      <label for="name" class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900">Price Range</label>
-      <input type="text" name="name" id="name" class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="$200k-$300k">
-    </div> -->
-    <button type="button" class="inline-flex justify-center items-center px-6 py-1.5 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-hover-300">
+    <button type="button" class="w-full inline-flex justify-center items-center px-6 py-1.5 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-hover-300">
       Get started
     </button>
   </div>
