1d5a03a449e01c8a59921218f9e1341be29e80b7
danwritecode <dnelson4993@outlook.com>
2022-01-25 19:43:46 -0500

    trying to create scrolling feature

---
+++ pages/testing.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bg-gray-200 py-96">
+    {{ testing }}
+  </div>
+  <div class="bg-gray-200 py-96">
+    {{ testing }}
+  </div>
+  <div class="bg-gray-200 py-96">
+    {{ testing }}
+  </div>
+</template>
+
+
+<script>
+import debounce from 'lodash/debounce'
+import { defineComponent, onMounted, beforeUnmount } from '@vue/composition-api'
+
+
+export default defineComponent ({
+  setup() {
+    const testing = "foo"
+
+    onMounted(() => {
+      // this.handleDebouncedScroll = debounce(handleScroll, 100);
+      window.addEventListener('scroll', handleScroll());
+    })
+
+    beforeUnmount(() => {
+      window.removeEventListener('scroll')
+    })
+
+    const handleScroll = (event) => {
+      console.log('scrolled')
+      console.log(event)
+    }
+
+    return {
+      testing
+    }
+  },
+})
+</script>
