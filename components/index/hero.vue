cc1fe1530bde4671cabc4438a61343b03fa03190
danwritecode <dnelson4993@outlook.com>
2022-02-01 20:43:07 -0500

    updated mobile formatting of hero

--- components/index/hero.vue
+++ components/index/hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative -mt-14 sm:-mt-32 flex justify-center items-center" style="min-height:65vh;">
+  <div class="lg:relative mt-10 sm:-mt-32 flex justify-center items-center" style="min-height:65vh;">
     <div class="grid grid-cols-1 gap-10 sm:gap-12 xl:gap-24 lg:grid-cols-2 items-center mt-10 mx-auto max-w-screen-xl">
       <div class="text-center sm:text-left -mt-32">
         <h3 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">One stop shop for all real estate needs</h3>
@@ -8,7 +8,7 @@
           <servicesselector />
         </div>
       </div>
-      <div class="hidden lg:relative lg:flex lg:justify-center sm:justify-end mt-10 sm:mt-16 md:mt-0">
+      <div class="lg:flex lg:justify-center sm:justify-end sm:mt-16 md:mt-0">
         <!-- <img src="@/assets/img/hero_home.jpg" alt="" class="rounded-lg"> -->
         <img src="@/assets/img/neighborhood.svg" alt="" class="rounded-lg">
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <IndexHero />
-  <IndexAbout class="mt-4 sm:mt-20"/>
+  <IndexAbout class="mt-12 sm:mt-20"/>
   <IndexVersus class="mt-20 sm:mt-32"/>
 </template>
