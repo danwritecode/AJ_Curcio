9928c43a6c151a009f0cc83e473e24ee04b2bbc1
danwritecode <dnelson4993@outlook.com>
2022-02-08 22:34:00 -0500

    added Alert/Succes alert component

--- components/getstarted/questionairefooter.vue
+++ components/getstarted/questionairefooter.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="px-4 py-3 bg-gray-50 text-right sm:px-6 border border-gray-200 rounded-b-md">
-    <ClientOnly>
-      <button @click="[$emit('nextStep'), formValid = false]" v-if="!onLastStep && formValid" type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-hover-300">
-        Next
-      </button>
-      <button v-else-if="!onLastStep && !formValid" type="button" disabled class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-red-200 bg-red-400 cursor-default">
-        Next
-      </button>
-      <GlobalSubmitButton v-else @click="onSubmit()" text="Submit" color="red" size="md" :loading="submitLoading" :disabled="!formValid" :invertedColor="false" />
-    </ClientOnly>
+  <div>
+    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6 border border-gray-200 rounded-b-md">
+      <ClientOnly>
+        <button @click="[$emit('nextStep'), formValid = false]" v-if="!onLastStep && formValid" type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-hover-300">
+          Next
+        </button>
+        <button v-else-if="!onLastStep && !formValid" type="button" disabled class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-red-200 bg-red-400 cursor-default">
+          Next
+        </button>
+        <GlobalSubmitButton v-else @click="onSubmit()" text="Submit" color="red" size="md" :loading="submitLoading" :disabled="!formValid || formSubmitted" :invertedColor="false" />
+      </ClientOnly>
+    </div>
+    <GlobalFormSubmitAlert v-if="formSubmitted" class="mt-6" :success="submitSuccess"/>
   </div>
 </template>
 
@@ -26,21 +29,26 @@ const props = defineProps({
 const emit = defineEmits(['nextStep'])
 
 const submitLoading = ref(false)
+const formSubmitted = ref(false)
+const submitSuccess = ref(false)
 
 const onSubmit = async () => {
   submitLoading.value = true
   try {
     const response = await $api.sendMessage(props.form)
+    submitSuccess.value = true
     console.log(response)
   } catch(err) {
     console.log(err)
   } finally {
     submitLoading.value = false
+    formSubmitted.value = true
   }
 }
 
 
 const formValid = ref(false)
+
 const emailValid = computed(() => {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(props.form.email)
