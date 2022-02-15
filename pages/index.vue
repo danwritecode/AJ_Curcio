2803de66e3f87931a3e3bb7f4e07fc91a1eaa47a
danwritecode <dnelson4993@outlook.com>
2022-02-14 20:36:13 -0500

    added padding to footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <IndexHero />
-    <IndexAbout class="mt-12 sm:mt-20"/>
-    <IndexVersus class="mt-20 sm:mt-32"/>
-    <IndexCta class="mt-20 sm:mt-32 pb-32"/>
+    <IndexAbout class="mt-12 sm:mt-20" />
+    <IndexVersus class="mt-20 sm:mt-32" />
+    <IndexCta class="mt-20 sm:mt-32 pb-32" />
   </div>
 </template>
