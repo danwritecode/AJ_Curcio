378dff7f3291c210961a7ae499dacc32cc1d871d
danwritecode <dnelson4993@outlook.com>
2022-03-04 08:39:34 -0500

    HOTFIX: modified quick contact form to not allow input after form submission

--- components/getstarted/quickmessage.vue
+++ components/getstarted/quickmessage.vue
@@ -4,21 +4,21 @@
       <div v-if="currentStep === 'Quick Contact'" class="px-4 py-5 bg-white sm:p-6">
         <div class="grid grid-cols-6 gap-6">
           <div class="col-span-6 flex flex-col-reverse">
-            <input v-model="form.fullName" type="text" name="full-name" id="fullName" autocomplete="given-name" required class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md transition-hover-300">
+            <input v-model="form.fullName" type="text" name="full-name" id="fullName" autocomplete="given-name" required :disabled="!allowInput" class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md transition-hover-300">
             <label for="first-name" class="block text-sm font-medium text-gray-700">Full Name</label>
           </div>
 
           <div class="col-span-6 sm:col-span-3 flex flex-col-reverse">
-            <input v-model="form.email" type="text" name="email-address" id="email" autocomplete="email" class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md transition-hover-300">
+            <input v-model="form.email" type="text" name="email-address" id="email" autocomplete="email" :disabled="!allowInput" class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md transition-hover-300">
             <label for="email-address" class="block text-sm font-medium text-gray-700">Email address</label>
           </div>
 
           <div class="col-span-6 sm:col-span-3 flex flex-col-reverse">
-            <input v-model="form.phone" type="text" name="phone" id="phone" autocomplete="email" required class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md transition-hover-300">
+            <input v-model="form.phone" type="text" name="phone" id="phone" autocomplete="email" required :disabled="!allowInput" class="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md transition-hover-300">
             <label for="phone" class="block text-sm font-medium text-gray-700">Phone Number</label>
           </div>
           <div class="col-span-6 flex flex-col-reverse">
-            <select v-model="form.serviceInterest" id="serviceInterest" name="service-interest" required class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm transition-hover-300">
+            <select v-model="form.serviceInterest" id="serviceInterest" name="service-interest" required :disabled="!allowInput" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm transition-hover-300">
               <option>Sell Home</option>
               <option>Buy Home</option>
               <option>Get a Mortgage</option>
